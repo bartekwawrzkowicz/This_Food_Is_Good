311877dac0a7cc60463cdf44eb11b0bb534fbd97
Bartosz <73258197+bartekwawrzkowicz@users.noreply.github.com>
2021-09-29 20:24:05 +0200

    fixing style names

--- src/pages/RecommendationsPage.js
+++ src/pages/RecommendationsPage.js
@@ -20,39 +20,39 @@ const RecommendationsPage = () => {
                 </div>
                 <div className="recommendation-heading">
                     <Link to={{ pathname: "http://www.sunsetbreeze.net" }} style={{ textDecoration: 'none' }} target="_blank" >
-                        <h2 className="restaurant-heading__header">{restaurantData[0].name}</h2>
+                        <h2 className="restaurant__name">{restaurantData[0].name}</h2>
                     </Link>
-                    <p className="restaurant-heading__text">{restaurantData[0].town}, {restaurantData[0].country}</p>
+                    <p className="restaurant__country">{restaurantData[0].town}, {restaurantData[0].country}</p>
 
                     <Link to={{ pathname: "http://www.versaillesrestaurant.com" }} style={{ textDecoration: 'none' }} target="_blank" >
-                        <h2 className="restaurant-heading__header">{restaurantData[4].name}</h2>
+                        <h2 className="restaurant__name">{restaurantData[4].name}</h2>
                     </Link>
-                    <p className="restaurant-heading__text">{restaurantData[4].town}, {restaurantData[4].country}</p>
+                    <p className="restaurant__country">{restaurantData[4].town}, {restaurantData[4].country}</p>
 
                     <Link to={{ pathname: "http://www.facebook.com/CudaWiankiPrzemysl" }} style={{ textDecoration: 'none' }} target="_blank" >
-                        <h2 className="restaurant-heading__header">{restaurantData[5].name}</h2>
+                        <h2 className="restaurant__name">{restaurantData[5].name}</h2>
                     </Link>
-                    <p className="restaurant-heading__text">{restaurantData[5].town}, {restaurantData[5].country}</p>
+                    <p className="restaurant__country">{restaurantData[5].town}, {restaurantData[5].country}</p>
 
                     <Link to={{ pathname: "https://restauracja-bohema.pl/" }} style={{ textDecoration: 'none' }} target="_blank" >
-                        <h2 className="restaurant-heading__header">{restaurantData[13].name}</h2>
+                        <h2 className="restaurant__name">{restaurantData[13].name}</h2>
                     </Link>
-                    <p className="restaurant-heading__text">{restaurantData[13].town}, {restaurantData[13].country}</p>
+                    <p className="restaurant__country">{restaurantData[13].town}, {restaurantData[13].country}</p>
 
                     <Link to={{ pathname: "https://alohapoke.eatbu.com/?lang=pl" }} style={{ textDecoration: 'none' }} target="_blank" >
-                        <h2 className="restaurant-heading__header">{restaurantData[6].name}</h2>
+                        <h2 className="restaurant__name">{restaurantData[6].name}</h2>
                     </Link>
-                    <p className="restaurant-heading__text">{restaurantData[6].town}, {restaurantData[6].country}</p>
+                    <p className="restaurant__country">{restaurantData[6].town}, {restaurantData[6].country}</p>
 
                     <Link to={{ pathname: "https://www.cheenhuaye.com" }} style={{ textDecoration: 'none' }} target="_blank" >
-                        <h2 className="restaurant-heading__header">{restaurantData[8].name}</h2>
+                        <h2 className="restaurant__name">{restaurantData[8].name}</h2>
                     </Link>
-                    <p className="restaurant-heading__text">{restaurantData[8].town}, {restaurantData[8].country}</p>
+                    <p className="restaurant__country">{restaurantData[8].town}, {restaurantData[8].country}</p>
 
                     <Link to={{ pathname: "https://www.dworzysko.pl" }} style={{ textDecoration: 'none' }} target="_blank" >
-                        <h2 className="restaurant-heading__header">{restaurantData[10].name}</h2>
+                        <h2 className="restaurant__name">{restaurantData[10].name}</h2>
                     </Link>
-                    <p className="restaurant-heading__text">{restaurantData[10].town}, {restaurantData[10].country}</p>
+                    <p className="restaurant__country">{restaurantData[10].town}, {restaurantData[10].country}</p>
                 </div>
 
                 <div className="recommendation-text">
