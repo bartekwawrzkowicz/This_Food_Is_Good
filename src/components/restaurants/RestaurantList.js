82e3c8c7560f3d441a9b87184b48652bab9dbaaf
Bartosz <73258197+bartekwawrzkowicz@users.noreply.github.com>
2021-09-29 20:18:12 +0200

    adding AnimateHeight

--- src/components/restaurants/RestaurantList.js
+++ src/components/restaurants/RestaurantList.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import AnimateHeight from 'react-animate-height';
 
 import restaurantData from '../../articles/restaurants.json';
 import Button from '../../layouts/Button';
@@ -20,71 +21,72 @@ const RestaurantList = props => {
                 <p className="restaurant__link">{restaurantData[props.number].www}</p>
                 <h2 className="restaurant__address">{restaurantData[props.number].address}</h2>
                 <p className="restaurant__country">{restaurantData[props.number].town}, {restaurantData[props.number].country}</p>
-
-                <ul className={nonActive ? "restaurants row non-active" : "restaurants row"}>
-                    <li className="restaurants__item">
-                        <div className="restaurants__item-image">
-                            <img src={restaurantData[props.number].images_first_dish} alt="first-dish" />
-                        </div>
-                        <article className="restaurants__item-article">
-                            {restaurantData[props.number].first_dish}
-                        </article>
-                    </li>
-
-                    <li className="restaurants__item">
-                        <div className="restaurants__item-image">
-                            <img src={restaurantData[props.number].images_second_dish} alt="second-dish" />
-                        </div>
-                        <article className="restaurants__item-article">
-                            {restaurantData[props.number].second_dish}
-                        </article>
-                    </li>
-
-                    <li className="restaurants__item">
-                        <div className="restaurants__item-image">
-                            {restaurantData[props.number].images_third_dish ?
-                                <img src={restaurantData[props.number].images_third_dish
-                                } alt="third-dish" /> : null}
-                        </div>
-                        {restaurantData[props.number].third_dish ?
+                <AnimateHeight height={nonActive ? 0 : "auto"} duration={1000} >
+                    <ul className="restaurants row">
+                        <li className="restaurants__item">
+                            <div className="restaurants__item-image">
+                                <img src={restaurantData[props.number].images_first_dish} alt="first-dish" />
+                            </div>
                             <article className="restaurants__item-article">
-                                <p>{restaurantData[props.number].third_dish}</p>
-                            </article> : null}
-                    </li>
+                                {restaurantData[props.number].first_dish}
+                            </article>
+                        </li>
 
-                    <li className="restaurants__item">
-                        <div className="restaurants__item-image">
-                            {restaurantData[props.number].images_fourth_dish ?
-                                <img src={restaurantData[props.number].images_fourth_dish
-                                } alt="fourth-dish" /> : null}
-                        </div>
-                        {restaurantData[props.number].fourth_dish ?
+                        <li className="restaurants__item">
+                            <div className="restaurants__item-image">
+                                <img src={restaurantData[props.number].images_second_dish} alt="second-dish" />
+                            </div>
                             <article className="restaurants__item-article">
-                                {restaurantData[props.number].fourth_dish}
-                            </article> : null}
-                    </li>
+                                {restaurantData[props.number].second_dish}
+                            </article>
+                        </li>
 
-                    <li className="restaurants__item">
-                        <div className="restaurants__item-image">
-                            {restaurantData[props.number].images_fifth_dish ?
-                                <img src={restaurantData[props.number].images_fifth_dish
-                                } alt="fifth-dish" /> : null}
+                        <li className="restaurants__item">
+                            <div className="restaurants__item-image">
+                                {restaurantData[props.number].images_third_dish ?
+                                    <img src={restaurantData[props.number].images_third_dish
+                                    } alt="third-dish" /> : null}
+                            </div>
+                            {restaurantData[props.number].third_dish ?
+                                <article className="restaurants__item-article">
+                                    <p>{restaurantData[props.number].third_dish}</p>
+                                </article> : null}
+                        </li>
+
+                        <li className="restaurants__item">
+                            <div className="restaurants__item-image">
+                                {restaurantData[props.number].images_fourth_dish ?
+                                    <img src={restaurantData[props.number].images_fourth_dish
+                                    } alt="fourth-dish" /> : null}
+                            </div>
+                            {restaurantData[props.number].fourth_dish ?
+                                <article className="restaurants__item-article">
+                                    {restaurantData[props.number].fourth_dish}
+                                </article> : null}
+                        </li>
+
+                        <li className="restaurants__item">
+                            <div className="restaurants__item-image">
+                                {restaurantData[props.number].images_fifth_dish ?
+                                    <img src={restaurantData[props.number].images_fifth_dish
+                                    } alt="fifth-dish" /> : null}
+                            </div>
+                            {restaurantData[props.number].fifth_dish ?
+                                <article className="restaurants__item-article">
+                                    {restaurantData[props.number].fifth_dish}
+                                </article> : null}
+                        </li>
+
+                        <div className="restaurant__footer">
+                            <article className="restaurant__footer-article">{restaurantData[props.number].text}</article>
                         </div>
-                        {restaurantData[props.number].fifth_dish ?
-                            <article className="restaurants__item-article">
-                                {restaurantData[props.number].fifth_dish}
-                            </article> : null}
-                    </li>
 
-                    <div className="restaurant__footer">
-                        <article className="restaurant__footer-article">{restaurantData[props.number].text}</article>
-                    </div>
+                    </ul>
 
-                </ul>
 
+                    <Button state={nonActive} click={buttonActiveHandler} />
+                </AnimateHeight>
             </div>
-            <Button state={nonActive} click={buttonActiveHandler} />
-
         </>
     )
 }
